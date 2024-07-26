5ae6a2a3eb3016efa81eab95ff80d4d3a0a0d910
jules <jpointil@student.42.fr>
2024-07-26 12:04:09 +0200

    parser en cours V2...

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: jules <jules@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 12:04:22 by jpointil          #+#    #+#             */
-/*   Updated: 2024/07/26 00:10:51 by jules            ###   ########.fr       */
+/*   Updated: 2024/07/26 11:51:38 by jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ t_redir *add_redir_node(t_token token, char *file)
 // 	// oui
 // }
 
-void	lex_loop(t_lex *lex, t_cmd *prev, t_cmd *cmd, int *i)
+void	lex_loop(t_lex *lex, t_cmd *prev, t_cmd *cmd)
 {
 	while (lex)
 	{
@@ -54,19 +54,25 @@ void	lex_loop(t_lex *lex, t_cmd *prev, t_cmd *cmd, int *i)
 		}
 		lex = lex->next;
 	}
+	cmd->next = NULL;
+	cmd->command[i] = NULL;
 }
 
-t_cmd	*rec_parse(t_lex *lex, t_cmd *prev, t_cmd *cmd, int *i)
+t_cmd	*rec_parse(t_lex *lex, t_cmd *prev, t_cmd *cmd)
 {
 	if (!lex)
 		return (cmd);
 	cmd = ft_calloc(sizeof(t_cmd));
 	if (!cmd)
-		// gestion erreur : free lex free cmd;
-		cmd->prev = prev;
-	lex_loop(lex, prev, cmd, &i);
-	cmd->next = NULL;
-	cmd->command[i] = NULL;
+		// gestion erreur : free lex, cmd;
+	cmd->commands = calloc(1, sizeof(char));
+	if (!cmd->commands)
+		//gestion erreur : free lex, cmd;
+	cmd->redir = calloc(1, sizeof(t_redir));
+	if (!cmd->redir)
+		//gestion erreur : free lex, cmd;
+	cmd->prev = prev;
+	lex_loop(lex, prev, cmd);
 }
 
 void	parser(t_shell *shell, t_lex *lex)
@@ -74,3 +80,37 @@ void	parser(t_shell *shell, t_lex *lex)
 	shell.cmd = rec_parse(lex, NULL, NULL, 0);
 	// free(lex);
 }
+
+/*fonctionnement rec parse :
+
+-prend la t_cmd verifie si lex et initialise un premier noeud.
+parcours de lex :
+
+check pour chaque noeud de lex le token :
+-si word ajoute a l'index du tableau command
+-sinon si pipe termine le tableau command, la liste redir,
+et relance la ft avec le nouveau noeud
+-sinon ajoute le noeud token a la liste redir
+---fin de lex 
+
+fin de fonction : 
+
+-ferme le dernier tableau command
+-mets le next du maillon a NULL
+renvoie le noeud actuel
+
+allocations :
+t_cmd :
+-allouer le noeud (calloc)
+commands :
+-calloc le tab de commands de la taille d'un char
+-calloc la command
+-avant de joinfree mettre le dernier char a '\n' pour le split
+-a chaque rajout strjoin_free avec la new command
+-commands = split bigline avec \n
+t_redir :
+-calloc t_redir
+-strdup de lex->next->word dans file
+-redir->next = NULL;
+
+*/
