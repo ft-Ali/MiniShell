3a409b3cb0526d3fbb202d3364e93bc00a4ea5a5
ft-jules <jpointil@42.student.fr>
2024-09-12 14:25:10 +0200

    lexer exit shell

--- srcs/lexer/tokenize.c
+++ srcs/lexer/tokenize.c
@@ -20,7 +20,7 @@ void	add_new_token(t_shell *shell, t_lex **lexer, int token_type)
 
 	new_token = ft_calloc(1, sizeof(t_lex));
 	if (!new_token)
-		exit_shell(shell, "lexer malloc");
+		exit_shell(shell, A_ERR);
 	new_token->word = NULL;
 	new_token->token = token_type;
 	if (!*lexer)
@@ -45,7 +45,7 @@ void	add_new_word(t_shell *shell, t_lex **lexer, char *str, int indices[2])
 
 	new_word = ft_calloc(1, sizeof(t_lex));
 	if (!new_word)
-		exit_shell(shell, "lexer malloc");
+		exit_shell(shell, A_ERR);
 	new_word->word = ft_strndup(str + indices[0] - indices[1], indices[1]);
 	new_word->token = 0;
 	if (!*lexer)
