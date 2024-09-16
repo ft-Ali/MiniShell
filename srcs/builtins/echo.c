83f184827d3aa3341a5ad758499e48a08c04d4be
jpointil <jpointil@student.42.fr>
2024-09-16 14:36:45 +0200

    echo finished

---
+++ srcs/builtins/echo.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpointil <jpointil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 13:20:42 by jpointil          #+#    #+#             */
+/*   Updated: 2024/09/16 14:36:25 by jpointil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	newline_check(char **cmd, int *valid)
+{
+	int w;
+	int c;
+
+    w = 1;
+    while (cmd[w] && cmd[w][0] == '-' && cmd[w][1] == 'n')
+    {
+        c = 1;
+        if (cmd[w][0] == '-' && cmd[w][1] == 'n')
+        {
+            while (cmd[w][c] == 'n')
+            c++;
+            if (cmd[w][c] != '\0')
+                return (w);
+        }
+        *valid = 1;
+        w++;
+    }
+    return (w);
+}
+
+void bt_echo(t_shell *shell, t_cmd *cmd)
+{
+    int new_line;
+    int i;
+
+    new_line = 0;
+    i = newline_check(cmd->comands, &new_line);
+    if (cmd->commands[1])
+    {
+        while (cmd->commands[i])
+        {
+            if (cmd->commands[i] && !cmd->commands[i + 1])
+                printf("%s", cmd->commands[i++]);
+            else if (cmd->commands[i])
+                printf("%s", cmd->commands[i++]);
+        }
+    }
+    if (!new_line)
+        printf("\n");
+    exit_shell(shell);
+}
