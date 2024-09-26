6b1425e586f0c5d1a01a791b349316ebcf105b11
jpointil <jpointil@student.42.fr>
2024-09-26 13:46:26 +0200

    expander OK

--- srcs/expander/expander_utils.c
+++ srcs/expander/expander_utils.c
@@ -6,7 +6,7 @@
 /*   By: jpointil <jpointil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 13:40:08 by alsiavos          #+#    #+#             */
-/*   Updated: 2024/09/26 12:41:48 by jpointil         ###   ########.fr       */
+/*   Updated: 2024/09/26 13:41:40 by jpointil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,6 +76,37 @@ char	*get_custom_env(t_env *env, char *var_name)
 // }
 
 // 2EME
+// int	handle_variable_expansion(t_expand *exp, char **result, char quote,
+// 		t_env *env)
+// {
+// 	char	*variable_name;
+// 	char	*variable_value;
+// 	int		start;
+
+// 	if (exp->input[exp->pos] == '$' && (quote != '\'' || quote != '\"'))
+// 	{
+// 		exp->pos++;
+// 		if (exp->input[exp->pos] == '\0' || (!ft_isalnum(exp->input[exp->pos])
+// 				&& exp->input[exp->pos] != '_'))
+// 		{
+// 			*result = ft_strjoin_free_n(*result, "$");
+// 			return (1);
+// 		}
+// 		start = exp->pos;
+// 		while (ft_isdigit(exp->input[exp->pos])
+// 			|| ft_isalpha(exp->input[exp->pos]) || exp->input[exp->pos] == '_')
+// 			exp->pos++;
+// 		variable_name = ft_strndup(exp->input + start, exp->pos - start);
+// 		variable_value = get_custom_env(env, variable_name);
+// 		if (variable_value)
+// 			*result = ft_strjoin_free_n(*result, variable_value);
+// 		free(variable_name);
+// 		return (1);
+// 	}
+// 	return (0);
+// }
+
+// 3EME
 int	handle_variable_expansion(t_expand *exp, char **result, char quote,
 		t_env *env)
 {
@@ -83,23 +114,21 @@ int	handle_variable_expansion(t_expand *exp, char **result, char quote,
 	char	*variable_value;
 	int		start;
 
-	if (exp->input[exp->pos] == '$' && (quote != '\'' || quote != '\"'))
+	if (exp->input[exp->pos] == '$' && quote != '\'')
 	{
 		exp->pos++;
 		if (exp->input[exp->pos] == '\0' || (!ft_isalnum(exp->input[exp->pos])
 				&& exp->input[exp->pos] != '_'))
-		{
-			*result = ft_strjoin_free_n(*result, "$");
-			return (1);
-		}
+			return (*result = ft_strjoin_free_n(*result, "$"), 1);
 		start = exp->pos;
-		while (ft_isdigit(exp->input[exp->pos])
-			|| ft_isalpha(exp->input[exp->pos]) || exp->input[exp->pos] == '_')
+		while (ft_isalnum(exp->input[exp->pos]) || exp->input[exp->pos] == '_')
 			exp->pos++;
 		variable_name = ft_strndup(exp->input + start, exp->pos - start);
 		variable_value = get_custom_env(env, variable_name);
 		if (variable_value)
 			*result = ft_strjoin_free_n(*result, variable_value);
+		else
+			*result = ft_strjoin_free_n(*result, "");
 		free(variable_name);
 		return (1);
 	}
